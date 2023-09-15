f2343a46bd0aa20c7ad43572de5a4f9e41a4f3d5
Cyril3012 <chandrucyril@gmail.com>
2023-09-15 13:05:58 +0530

    modified small change in pattern clss

--- src/controlStatements/Pattern.java
+++ src/controlStatements/Pattern.java
@@ -1,31 +1,40 @@
 package controlStatements;
+
 import java.util.Scanner;
-
-
 
 public class Pattern {
 
 	public static void main(String[] args) {
 		// triangle pattern using class and method
-		
-		Scanner sc = new Scanner(System.in);  
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		int num = sc.nextInt();
-		int i,j,k=0;
-		for( i =0 ;i<num;i++) {
-		
-			for(j=num;j>i;j--) {
+		int i, j, k = 0;
+		for (i = 0; i < num; i++) {
+
+			for (j = num; j > i; j--) {
 				System.out.print(" ");
 			}
-			
-			for(k=0; k<i+1;k++) { 
+
+			for (k = 0; k < i + 1; k++) {
 				System.out.print("* ");
 			}
 			System.out.println("");
 		}
-/*----second half using while loop----------*/
-		
-		
+		/*----second half using while loop----------*/
+		for (i = 0; i < num; i++) {
+
+			for (j = num; j > i; j--) {
+				System.out.print(" ");
+			}
+
+			for (k = 0; k < i + 1; k++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+
 	}
 
 }
